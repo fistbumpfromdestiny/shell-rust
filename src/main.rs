1f66d808293972dd9893f5adff669ba334ed3c4b
Fredrik Larsson <fredrik@wizardry.se>
2026-01-16 12:00:47 +0100

    Fix test failure

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    let command = io::stdin().read_line(&mut command).unwrap();
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    let command = command.trim();
     println!("{command}: not found");
         
 }
