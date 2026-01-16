1a4bdba5ee4080424ca79ff48f2f724d092a55f3
Fredrik Larsson <fredrik@wizardry.se>
2026-01-16 15:17:54 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::io::{self, Write};
 
 fn main() {
+    let builtins = vec!["exit", "echo", "type"];
     loop {
         let mut command = String::new();
         print!("$ ");
@@ -17,6 +18,7 @@ fn main() {
                 match trimmed.split_once(' ') {
                     Some(("exit", _)) => return,
                     Some(("echo", args)) => echo(args),
+                    Some(("type", args)) => find_type(args, &builtins),
                     Some((cmd, _)) => println!("{cmd}: command not found"),
                     None if trimmed == "exit" => return,
                     None => println!("{trimmed}: command not found"),
@@ -27,7 +29,18 @@ fn main() {
     }
 }
 
-fn echo(command: 
-    &str) {
-    println!("{command}");
+fn echo(args: &str) {
+    println!("{args}");
+}
+
+fn find_type(args: &str, builtins: &Vec<&str>) {
+    let args: Vec<&str> = args.trim().split_whitespace().collect();
+    for arg in &args {
+        if builtins.contains(arg) {
+            println!("{arg} is a shell builtin");
+        } else {
+            println!("{arg}: not found")
+        }
+    }
+    
 }
